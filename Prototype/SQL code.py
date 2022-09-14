9240bcf2c0a240b47ce8699f126f00864c4903a5
PranavManjuladeviUdayakumar <PranavManjuladeviUdayakumar@gmail.com>
2022-09-14 22:03:05 +0530

    reorganize without gui

--- Prototype/SQL code.py
+++
@@ -1,7 +0,0 @@
-if input("Retrieve house information from MySQL? (y/n): ").lower() == 'y':
-    mc = m.connect(host= '127.0.0.1', user= 'root', password= 'amaatra', database= 'hapsprice')
-    cur = mc.cursor()
-    cur.execute(f"select * from data where row = {row} and col = {col}")
-    data = cur.fetchall()
-    for i in data:
-        print(i)
