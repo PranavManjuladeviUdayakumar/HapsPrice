77d41c1d67acd4393807cfd970239d159e6d21b5
PranavManjuladeviUdayakumar <PranavManjuladeviUdayakumar@gmail.com>
2022-09-20 23:32:15 +0530

    Updated MySQL command v1.3

--- HapsPrice.py
+++ HapsPrice.py
@@ -110,7 +110,8 @@ print("House Price with current modifiers is: ",
 if input("Retrieve house information from MySQL? (y/n): ").lower() == 'y':
     mc = m.connect(host= '127.0.0.1', user= 'root', password= 'amaatra', database= 'hapsprice')
     cur = mc.cursor()
-    cur.execute(f"select * from data where row = {row} and col = {col}")
+    cur.execute(f"select * from data")
     data = cur.fetchall()
     for i in data:
-        print(i)
+        if i[0] == row and i[1] == col:
+            print(i)
